6ddf4715d1a5cd760a9c15dfe189ca3cc1cf7729
negativeExponent <negativeExponent@users.noreply.github.com>
2019-12-17 14:13:48 +0800

    Cleanup Makefile

--- mednafen/settings.h
+++ mednafen/settings.h
@@ -1,7 +1,6 @@
 #ifndef MDFN_SETTINGS_H
 #define MDFN_SETTINGS_H
 
-#if defined(WANT_PCE_FAST_EMU)
 extern bool setting_pce_fast_forcesgx;
 extern bool setting_pce_fast_nospritelimit;
 extern int setting_initial_scanline;
@@ -14,15 +13,6 @@ extern int setting_pce_fast_cdpsgvolume;
 extern uint32_t setting_pce_fast_cdspeed;
 extern std::string setting_pce_fast_cdbios;
 extern bool OrderOfGriffonFix;
-#elif defined(WANT_NGP_EMU)
-extern uint32_t setting_ngp_language;
-#elif defined(WANT_GBA_EMU)
-extern uint32_t setting_gba_hle;
-#elif defined(WANT_VB_EMU)
-extern uint32_t setting_vb_lcolor;
-extern uint32_t setting_vb_rcolor;
-extern uint32_t setting_vb_anaglyph_preset;
-#endif
 
 bool MDFN_LoadSettings(const char *path, const char *section = NULL, bool override = false);
 bool MDFN_MergeSettings(const void*);
